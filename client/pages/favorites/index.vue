51ff6f34a0b51dbf398629dad66bff084252fcc9
giankyle989 <kylelaguerta989@gmail.com>
2024-04-03 17:32:31 +0800

    feat: display SpaceX Launches

---
+++ client/pages/favorites/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Favorites</h1>
+      <p>This is the about page. Here you can learn more about our company.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Add any necessary script logic here
+  }
+  </script>
